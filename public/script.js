298bae7338eab5aea389bc4f0de44b2e1efeb5e1
GeeDG <gabriele.dipinto03@gmail.com>
2026-05-05 20:04:38 +0200

    feat: automatic UI refreshing

--- public/script.js
+++ public/script.js
@@ -3,7 +3,27 @@ const playersList = document.querySelector("#players-list");
 const playerNameInput = document.querySelector("#player-name-input");
 const addPlayerButton = document.querySelector("#add-player-button");
 
-loadPlayersButton.addEventListener("click", async () => {
+loadPlayersButton.addEventListener("click", loadPlayers);
+
+addPlayerButton.addEventListener("click", async () => {
+  const playerName = playerNameInput.value;
+
+  await fetch("/api/players", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: playerName
+    })
+  });
+
+  playerNameInput.value = "";
+
+  await loadPlayers();
+});
+
+async function loadPlayers() {
   const response = await fetch("/api/players");
   const players = await response.json();
 
@@ -14,24 +34,4 @@ loadPlayersButton.addEventListener("click", async () => {
     listItem.textContent = `${player.name}: ${player.rating}`;
     playersList.appendChild(listItem);
   });
-});
-
-addPlayerButton.addEventListener("click", async () => {
-  const playerName = playerNameInput.value;
-
-  const response = await fetch("/api/players", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: playerName
-    })
-  });
-
-  const newPlayer = await response.json();
-
-  playerNameInput.value = "";
-
-  console.log(newPlayer);
-});
+}
