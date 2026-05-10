3474cca97c9e3f6f5cb6b6d3dd596aeeb6e3b845
Gianluca Vincenzo Digiacomo <gianluca.digiacomo04@gmail.com>
2026-05-10 21:56:26 +0200

    feat: aggiunto oggetto player e visualizzazione base su App

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container, Table } from "react-bootstrap";
+
+import { Player } from "./models/PingPongModels.js";
+
+function App() {
+  const [players, setPlayers] = useState([
+    new Player("Crazyguitar54"),
+    new Player("GabboTheGabbix"),
+    new Player("AlbiConLaClay"),
+    new Player("Federico Colombo")
+  ]);
+
+  // generata da chat, per giusto si dovrebbe fare un modulo a parte e fare navigare tra le pagine con react-route
+  return (
+    <Container className="mt-4">
+      <h1>Ping Pong Elo Tracker</h1>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nickname</th>
+            <th>Elo</th>
+            <th>Games played</th>
+            <th>Wins</th>
+            <th>Losses</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {players.map((player) => (
+            <tr key={player.nickname}>
+              <td>{player.nickname}</td>ù+ù++ù
+              <td>{player.elo}</td>
+              <td>{player.gamesPlayed}</td>
+              <td>{player.wins}</td>
+              <td>{player.losses}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+}
+
+export default App;
